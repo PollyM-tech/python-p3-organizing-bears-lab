25b61043e1dbb3a0f65775ec217b645f904062ba
Pauline Moraa <pmoraa431@gmail.com>
2025-05-21 14:49:32 +0300

    Perform CRUD operations on relational databases using SQL.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 20, 'M', 'dark brown', 'friendly', 1),
+('Rowdy', 10, 'M', 'black', 'playful', 1),
+('Tabitha', 6, 'F', 'brown', 'gentle', 1),
+('Sergeant Brown', 19, 'M', 'brown', 'strict', 1),
+('Melissa', 13, 'F', 'light brown', 'curious', 1),
+('Grinch', 2, 'M', 'green', 'grumpy', 1),
+('Wendy', 6, 'F', 'black', 'energetic', 1),
+(NULL, NULL, NULL, NULL, NULL, NULL);
